812f29ff0bd5bec6c8b284529d259363e5a94afb
Waheed El Miladi <waheed@intercom.io>
2016-10-23 09:39:52 +0100

    Update CI settings

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './stylesheets/App.css';
 
 class App extends Component {
-  componentDidMount() {
-
-  }
   render() {
     return (
       <div className="App">
