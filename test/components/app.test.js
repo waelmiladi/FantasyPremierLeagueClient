6d2cbd9e23d4dfe2c8db7a7b53add98535907ce1
Waheed El Miladi <waheed@intercom.io>
2016-10-26 19:15:00 +0100

    Fix tests

--- test/components/app.test.js
+++ test/components/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/app';
+import App from '../../src/components/app';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
