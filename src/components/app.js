df5fdf2439ee96c7df4e7569ff0a1a3add06c9ef
Waheed El Miladi <waheed@intercom.io>
2016-10-26 17:41:23 +0100

    React app

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentDidMount() {
+    fetch('http://localhost:8080/fpl', {
+      method: "POST",
+      body: "{ teams { id, name } }"
+    }).then((response) => {
+      response.json().then((data) => data);
+    });
+  }
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default App;
